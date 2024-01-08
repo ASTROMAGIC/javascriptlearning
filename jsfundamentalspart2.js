814567356401ff8e2e4a0be6ce32397cfb867a11
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-01-07 20:32:58 -0800

    learned about numbers, bigInt, booleans, bigInt, and null value types

--- jsfundamentalspart2.js
+++ jsfundamentalspart2.js
@@ -40,3 +40,73 @@ const greeting = `Hello, ${title}`;
 console.log(greeting);
 
 // i'll leave off here. pick up tomorrow at embedding javascript 
+
+
+// continuation - january 7th 
+
+// the first data type in javascript is a number. here is an example 
+
+let message = "hello";
+message = 12356; 
+
+// the variable message can exist as a string or a number. here is another example 
+
+let n = 123; 
+n = 12.345; 
+
+// the number type represents both integrer and floating point numbers. There are many operations numbers, e.g. multiplcation *, division /, addition +, subtraction -, and so on. 
+
+// beside regular numbers, there are "special numeric values" which also belong tot his data type: infitnity, -infinity, and NaN (not a number)
+
+alert(1/0); // gives infinity 
+alert(Infinity); // references infinity directly
+
+// NaN represents a computional error. it is a result of an incorrect or an undefinited mathematic operation. For example: 
+
+alert("not a number" / 2); // Nan, such division is erroneous. 
+
+// Remember that NaN is ticky. Any further mathematical operation on NaN returns NaN. 
+
+alert(NaN + 1); // returns NaN
+alert(3 * NaN); // returns NaN
+alert ("not a number" / 2-1); //returns NaN
+
+// in essence, if there's a NaN somewhere in a mathematical expression, it propagates to the whole result. The only exception being that NaN ** 0 is 1. 
+
+// is js, the "number" type cannot safely represent integer values larger than (2^53-1) or (-2^53-1) for negatives. Going beyond these limits will result in a precision error, because not all digits fit into the fixed 64-bit stoage. 
+
+// BigInt type was added to the js language to represent integers of arbitrary length. 
+
+// A BigInt value is created by appending n to the end of an integer: 
+
+const bigInt = 123456789876543213456798n; 
+
+// BigInt numbers are rarely needed, so look deeper into these when a project or work you will create will have a need for it. 
+
+// the next data type is a string, surround by the different types of quotes you went over earlier in this exercise. 
+
+// the next data type is a Boolean (the logical type.)
+
+//Boolean type only has two values: true and false. This type is commonly used to store yes/no values: true means "yes/correct", and false means "no/incorrect".
+
+let nameFieldCheck = true; // yes, name field is checked 
+let ageFieldChecked = false; //no, age field is not checked 
+
+// Boolean values can also come as a result of comparisons
+
+let isGreater = 4 > 1; 
+alert( isGreater ); // true (the comparison result is "yes")
+
+// the next data type is the "null" value 
+
+// the special null value does not belong to any of the types descibed above. 
+
+// it forms a seperate type  of its own which contains only the null value:
+
+let age = null; // in js, null is not a "reference to a non-existing object" or a "null pointer" like in some other languages. 
+
+// it is simply a special value which represents "nothing", "empty", or "value unknown"
+
+// the code above indicates that the variable age is unknown. 
+
+// the next data type in js 
