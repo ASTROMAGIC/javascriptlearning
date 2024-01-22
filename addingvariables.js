03621d7de7cda94c832821678283857818980c56
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-01-22 09:17:13 -0800

    finished adding variables ex
    completed two exercises on adding variables

---
+++ addingvariables.js
@@ -0,0 +1,13 @@
+const jay = 5;
+
+const jan = 5;
+
+let result;
+
+result = jay + jan; 
+
+return result; 
+
+
+
+
--- replitexercise1.js
+++ replitexercise1.js
@@ -6,9 +6,18 @@ function troubleshooting() {
 
 	// Edit between these lines
 	// =================================
-	result = "a" + "b";
+	result = a + b;
 
 	// =================================
 
 	return result;
 }
+
+
+// initially i believe this is equal to 0 because the value of both of these strings is 0 and in essence i'm adding two strings that have no value assigned to them, despite creating two variables with the numeric value of 1. 
+
+// if i remove the quotes from a and b, they will be variables and and will likely give me the correct result. 
+
+// ok that did not work and i'm not sure why. i think i'll google search this and look deeper into how operators work.
+
+// so it turns out i was right and that code=0 means the code return without errors. 
