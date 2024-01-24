7ac6e57c31ecb464032073941f20e1e3e5356bad
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-01-24 13:45:46 -0800

    replit complete
    completed and added all replit assignments

---
+++ replitassignment2.js
@@ -0,0 +1,10 @@
+const a = 1 + 8; 
+const b = 22 * 3; 
+const c = 5 % 4; 
+// ^ this is the remainder of 5/4, with the output of this being 1
+const d = a - 17;
+const e = a + b + c + d; 
+
+module.exports = {a, b, c, d, e}
+
+// replace the variable strings with the expressions they describe, easy enough. 
---
+++ replitexecise4.js
@@ -0,0 +1,17 @@
+
+
+number = Number(prompt('enter a number'));
+
+function numberChecker() {
+    if(number === 6) {
+        return true; 
+    }  else {
+        return false; 
+    }
+}
+
+// if i type it myself, the code appears with no errors but if i copy it from somewhere else the code might not work. Probably a formatting issue. I need to become more familiar with how functions and conditionals are supposed to work 
+
+// these things are only going to get get bigger and more complicated. But as far as I can read, this function is just a numerical verifier. From what I previously learned, as long as the number is the same type, this should return true. 
+
+// there isn't much more i can do with this file in this form, but i do need to figure out how to read the outputs of these exercises in the output viewer. might be some for some googling.
