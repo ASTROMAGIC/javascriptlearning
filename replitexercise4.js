361b2672e220550366c207ff2ca70e140d0c964b
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-01-24 14:01:42 -0800

    completed replt exercise 4
    string concatenation needs work for sure, need to be ok with going back and checking work constantly

---
+++ replitexercise4.js
@@ -0,0 +1,12 @@
+const birthYear = 1948; 
+const thisYear = 1965; 
+const firstName = "Carlos"; 
+const lastName = "Stevenson";
+
+const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old. "; 
+
+// this is four variables being called, with a final variable combining them together into a greeting using string concatenation. 
+
+// the outcome should be that the person tells you who they are and how old they are. 
+
+console.log(greeting); 
---
+++ replitexercise5.js
@@ -0,0 +1,10 @@
+const firstName = "Hakeem";
+const lastName = "Fasa";
+const thisYear = 2024; 
+const birthYear = 1995; 
+
+const greeting = " Hello my name is " + firstName + " " + lastName  + " and I am " + (thisYear - birthYear) + " years old!";
+
+console.log(greeting)
+
+// I should probably work on my string concatenation a bit, but this wasn't too hard. I think I need to let go of the idea of getting things immediately. The eseence of learning to code properly should involve me going back to check my work and trouble shooting a lot until I get things right. 
