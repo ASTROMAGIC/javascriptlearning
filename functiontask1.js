302596389ee116a221f82980d0fa28a8a083940a
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-02-29 10:37:46 -0800

    function task 2
    doing some more ground work on the second function task, this one seems a little easier than the first but still figuring it out

--- functiontask1.js
+++ functiontask1.js
@@ -13,3 +13,21 @@ console.log(random_Name(names));
 // this would be the function as concise as possible in the context of the html file. 
 
 // i want to write a function that a prints a random name from the provided array to the provided paragraph. 
+
+
+// create a function that draws a rectangle on a provided canvas element, based on these five provided input variables 
+
+// 
+//x — the x coordinate of the rectangle.
+//y — the y coordinate of the rectangle.
+//width — the width of the rectangle.
+//height — the height of the rectangle.
+//color — the color of the rectangle.
+
+function draw () {
+    let x = 50; 
+    let y = 100;
+    let width = 90; 
+    let height = 60; 
+    let color = green;
+}
