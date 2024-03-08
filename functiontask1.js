8e86ecc9cb4b04661a83bf585b411bf66435f97d
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-03-07 23:44:46 -0800

    proper rectangle function
    made proper rectangle function based on mdn web docs

--- functiontask1.js
+++ functiontask1.js
@@ -25,9 +25,11 @@ console.log(random_Name(names));
 //color — the color of the rectangle.
 
 function draw () {
-    let x = 50; 
-    let y = 100;
-    let width = 90; 
-    let height = 60; 
-    let color = green;
+    const canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+        ctx.fillRect(25, 25, 100, 100);
+        ctx.clearRect(45, 45, 60, 60);
+        ctx.strokeRect(50, 50, 50, 50);
+    }
 }
