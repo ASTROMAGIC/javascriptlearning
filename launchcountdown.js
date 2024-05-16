04504fd7f312a999e157b2a293da721eb2d69503
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-05-16 15:46:30 -0700

    launch countdown
    a basic for loop counting down from 10 and printing "Blast off" when it's completed. I understand this in principle.

---
+++ launchcountdown.js
@@ -0,0 +1,17 @@
+// this is a learning assignment from mdnwebdocs. 
+
+// loop from 10 down to 0. We've provided you with aninitializer - let i = 10;
+
+// the goal of this learning eexercise is to print out a simple luanch countdown to the output box, from 10 down to blastoff. 
+
+
+
+// remember the 3 parts of a loop: initializer, condition, and iteration
+
+for (let i =10; i >=0; i--) {
+    console.log(i);
+
+    if (i === 0) {
+        console.log("Blast Off!");
+    }
+}
