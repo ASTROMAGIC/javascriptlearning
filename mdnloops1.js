82eda57096b57db43ba8a5597bcb9cf478762447
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-05-21 17:25:37 -0700

    created simple loop to print array
    will need to create a new project file specifically for this to see it and get the proper result

--- mdnloops1.js
+++ mdnloops1.js
@@ -1,9 +1,11 @@
 // write a simple loop that goes through all the items in an array and prints them out on the screen inside list items. 
 
-const myArray = ["Supra", "M240i", "Z8", "Type-R", "Lotus"];
+const cars = ["Supra", "M240i", "Z8", "Type-R", "Lotus"];
 
-const list = document.querySelector("li");
 
-for (const cars of myArray) {
-    console.log(cars);
+
+for (const car of cars) {
+    console.log(car);
 }
+
+// this is the simplest way of writing, I just need to see how it would work being attached to the list item as described by the assignment. Will open a new project to do this correctly and see it in action 
