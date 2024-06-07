a6e1358e51557c9d1760c6b187f00058c05ee5c3
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-06-07 10:36:59 -0700

    shopping list mdn exercise
    began shopping list mdn exercise. i want to try my best to understand this the best i can on my own. will try to use supporting tools as little as possible

---
+++ mdnactivelearning/mdnshoppinglist.js
@@ -0,0 +1,8 @@
+// make a simple shopping list example that allows you to dynamically additems to the list using a form input and button. when you add an item to the input and press the button 
+
+// the item should appear in the list 
+
+// each item should be given a button that can be pressed to delete that item off the list 
+
+// the input should be empited and focused ready for you to enter another 
+
