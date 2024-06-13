86a058ed5199e44230fd6eefbbbdc393030cc009
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-06-13 11:08:13 -0700

    updated variables and delete function
    updated variables and the delete function, learned better written structure for javascript files

--- mdnactivelearning/mdnshoppinglist.js
+++ mdnactivelearning/mdnshoppinglist.js
@@ -6,37 +6,49 @@
 
 // the input should be empited and focused ready for you to enter another 
 
-const h1 = document.querySelector("h1");
+const list = document.querySelector("ul");
 
-const div = document.querySelector("div");
+const input = document.querySelector("input");
 
 const button = document.querySelector("button");
 
-button.addEventListener("click", ()=>{
-    let initialValue = document.getElementById("input");
-    initialValue = '';
+// so first off, most of my variables are off. I should have made elements for the unordered list, input, and the button to click. I made one for the heading??? I'm still to fearful to actually look at the document and intuitively decide the changes based on what I know. 
+
+// for every piece of javascript I add here, there should be HTML that already exists for which it is supporting. 
+
+button.addEventListener("click", ()=> {
+    const myItem = input.value;
+    input.value = '';
 
 })
+
+// the initial idea for the my button event listener was somewhat right, but I did not know that an input value for a variable could be written like this 
 
 const listItem = document.createElement("li");
+const listText = document.createElement("span");
+const listBtn = document.createElement("button");
 
-const span = document.createElement("span");
-span.textContent = initialValue;
+// I see that it is a good idea to group my variables that are being called together at the same time. It may also be a good idea to change the name of the variables together once I get a better idea of how the javascript file will look in context. 
 
-const buttonElement = document.createElement("button");
-buttonElement.textContent = "Delete";
 
-listItem.appendChild(span);
-listItem.appendChild(buttonElement);
+listBtn.textContent = "Delete";
 
-div.appendChild(listItem);
+listItem.appendChild(listText);
+list.textContent = myItem;
+listItem.appendChild(listBtn);
+listBtn.textContent = "Delete";
+list.appendChild(listItem);
 
-buttonElement.addEventListener("click", ()=>{
-    listItem.remove()
-})
 
-document.getElementById("input").addEventListener("click", () =>{
-    document.getElementById("Add item").focus();
-})
+
+listBtn.addEventListener("click", ()=>{
+    list.remove(listItem);
+});
+
+input.focus();
 
 // this is my first attempt and this dynamic shopping list. I will put this on github and compare with a proper solution. 
+
+// ok the added items are not showing up 
+
+// update I have update the code to matchup will the solution, let's see if it works. 
