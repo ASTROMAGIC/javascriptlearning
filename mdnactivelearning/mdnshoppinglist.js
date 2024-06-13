a05379a70c9e10d69fbd172aa58fe0b5c4888d56
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-06-13 10:52:44 -0700

    mdnshopping list complete
    first iteration for the code for a dynamic shopping list, will compare with proper solution then make appropriate changes.

--- mdnactivelearning/mdnshoppinglist.js
+++ mdnactivelearning/mdnshoppinglist.js
@@ -6,3 +6,37 @@
 
 // the input should be empited and focused ready for you to enter another 
 
+const h1 = document.querySelector("h1");
+
+const div = document.querySelector("div");
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", ()=>{
+    let initialValue = document.getElementById("input");
+    initialValue = '';
+
+})
+
+const listItem = document.createElement("li");
+
+const span = document.createElement("span");
+span.textContent = initialValue;
+
+const buttonElement = document.createElement("button");
+buttonElement.textContent = "Delete";
+
+listItem.appendChild(span);
+listItem.appendChild(buttonElement);
+
+div.appendChild(listItem);
+
+buttonElement.addEventListener("click", ()=>{
+    listItem.remove()
+})
+
+document.getElementById("input").addEventListener("click", () =>{
+    document.getElementById("Add item").focus();
+})
+
+// this is my first attempt and this dynamic shopping list. I will put this on github and compare with a proper solution. 
